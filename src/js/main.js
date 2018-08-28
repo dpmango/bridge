8ee6a4f75c4e919dd4d5924c99f1eab2cdcc3513
Sergey Khmelevskoy <xs290@me.com>
2018-08-28 15:03:35 +0300

    init preloader and home screen

--- src/js/main.js
+++ src/js/main.js
@@ -12,38 +12,17 @@ $(document).ready(function(){
   ////////////
   function pageReady(){
     legacySupport();
+    initPreloader();
     updateHeaderActiveClass();
     initHeaderScroll();
 
-    initPopups();
     initSliders();
-    initScrollMonitor();
-    initMasks();
-    initSelectric();
-    initValidations();
-
     // development helper
     _window.on('resize', debounce(setBreakpoint, 200))
-
-    // AVAILABLE in _components folder
-    // copy paste in main.js and initialize here
-    // initPerfectScrollbar();
-    // initLazyLoad();
-    // initTeleport();
-    // parseSvg();
-    // revealFooter();
-    // _window.on('resize', throttle(revealFooter, 100));
   }
 
   // this is a master function which should have all functionality
   pageReady();
-
-
-  // some plugins work best with onload triggers
-  _window.on('load', function(){
-    // your functions
-  })
-
 
   //////////
   // COMMON
@@ -74,6 +53,12 @@ $(document).ready(function(){
       return false;
     })
 
+
+  function initPreloader(){
+    setTimeout(function(){
+      $('.preloader').addClass('is-loaded')
+    }, 1500)
+  }
 
   // HEADER SCROLL
   // add .header-static for .page or body
@@ -131,274 +116,15 @@ $(document).ready(function(){
 
   function initSliders(){
 
-    // EXAMPLE SWIPER
-    new Swiper('[js-slider]', {
-      wrapperClass: "swiper-wrapper",
-      slideClass: "example-slide",
-      direction: 'horizontal',
-      loop: false,
-      watchOverflow: true,
-      setWrapperSize: false,
-      spaceBetween: 0,
-      slidesPerView: 'auto',
-      // loop: true,
-      normalizeSlideIndex: true,
-      // centeredSlides: true,
-      freeMode: true,
-      // effect: 'fade',
-      autoplay: {
-        delay: 5000,
-      },
-      navigation: {
-        nextEl: '.example-next',
-        prevEl: '.example-prev',
-      },
-      breakpoints: {
-        // when window width is <= 992px
-        992: {
-          autoHeight: true
-        }
-      }
-    })
+    // HOMEPAGE SLIDER
+
 
   }
 
-  //////////
-  // MODALS
-  //////////
-
-  function initPopups(){
-    // Magnific Popup
-    var startWindowScroll = 0;
-    $('[js-popup]').magnificPopup({
-      type: 'inline',
-      fixedContentPos: true,
-      fixedBgPos: true,
-      overflowY: 'auto',
-      closeBtnInside: true,
-      preloader: false,
-      midClick: true,
-      removalDelay: 300,
-      mainClass: 'popup-buble',
-      callbacks: {
-        beforeOpen: function() {
-          startWindowScroll = _window.scrollTop();
-          // $('html').addClass('mfp-helper');
-        },
-        close: function() {
-          // $('html').removeClass('mfp-helper');
-          _window.scrollTop(startWindowScroll);
-        }
-      }
-    });
-
-    $('[js-popup-gallery]').magnificPopup({
-  		delegate: 'a',
-  		type: 'image',
-  		tLoading: 'Загрузка #%curr%...',
-  		mainClass: 'popup-buble',
-  		gallery: {
-  			enabled: true,
-  			navigateByImgClick: true,
-  			preload: [0,1]
-  		},
-  		image: {
-  			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-  		}
-  	});
-  }
-
-  function closeMfp(){
-    $.magnificPopup.close();
-  }
 
   ////////////
   // UI
   ////////////
-
-  // textarea autoExpand
-  _document
-    .one('focus.autoExpand', '.ui-group textarea', function(){
-        var savedValue = this.value;
-        this.value = '';
-        this.baseScrollHeight = this.scrollHeight;
-        this.value = savedValue;
-    })
-    .on('input.autoExpand', '.ui-group textarea', function(){
-        var minRows = this.getAttribute('data-min-rows')|0, rows;
-        this.rows = minRows;
-        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-        this.rows = minRows + rows;
-    });
-
-  // Masked input
-  function initMasks(){
-    $("[js-dateMask]").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
-    $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
-  }
-
-  // selectric
-  function initSelectric(){
-    $('select').selectric({
-      maxHeight: 300,
-      arrowButtonMarkup: '<b class="button"><svg class="ico ico-select-down"><use xlink:href="img/sprite.svg#ico-select-down"></use></svg></b>',
-
-      onInit: function(element, data){
-        var $elm = $(element),
-            $wrapper = $elm.closest('.' + data.classes.wrapper);
-
-        $wrapper.find('.label').html($elm.attr('placeholder'));
-      },
-      onBeforeOpen: function(element, data){
-        var $elm = $(element),
-            $wrapper = $elm.closest('.' + data.classes.wrapper);
-
-        $wrapper.find('.label').data('value', $wrapper.find('.label').html()).html($elm.attr('placeholder'));
-      },
-      onBeforeClose: function(element, data){
-        var $elm = $(element),
-            $wrapper = $elm.closest('.' + data.classes.wrapper);
-
-        $wrapper.find('.label').html($wrapper.find('.label').data('value'));
-      }
-    });
-  }
-
-  ////////////
-  // SCROLLMONITOR - WOW LIKE
-  ////////////
-  function initScrollMonitor(){
-    $('.wow').each(function(i, el){
-
-      var elWatcher = scrollMonitor.create( $(el) );
-
-      var delay;
-      if ( $(window).width() < 768 ){
-        delay = 0
-      } else {
-        delay = $(el).data('animation-delay');
-      }
-
-      var animationClass = $(el).data('animation-class') || "wowFadeUp"
-
-      var animationName = $(el).data('animation-name') || "wowFade"
-
-      elWatcher.enterViewport(throttle(function() {
-        $(el).addClass(animationClass);
-        $(el).css({
-          'animation-name': animationName,
-          'animation-delay': delay,
-          'visibility': 'visible'
-        });
-      }, 100, {
-        'leading': true
-      }));
-      // elWatcher.exitViewport(throttle(function() {
-      //   $(el).removeClass(animationClass);
-      //   $(el).css({
-      //     'animation-name': 'none',
-      //     'animation-delay': 0,
-      //     'visibility': 'hidden'
-      //   });
-      // }, 100));
-    });
-
-  }
-
-  ////////////////
-  // FORM VALIDATIONS
-  ////////////////
-
-  // jQuery validate plugin
-  // https://jqueryvalidation.org
-  function initValidations(){
-    // GENERIC FUNCTIONS
-    var validateErrorPlacement = function(error, element) {
-      error.addClass('ui-input__validation');
-      error.appendTo(element.parent("div"));
-    }
-    var validateHighlight = function(element) {
-      $(element).parent('div').addClass("has-error");
-    }
-    var validateUnhighlight = function(element) {
-      $(element).parent('div').removeClass("has-error");
-    }
-    var validateSubmitHandler = function(form) {
-      $(form).addClass('loading');
-      $.ajax({
-        type: "POST",
-        url: $(form).attr('action'),
-        data: $(form).serialize(),
-        success: function(response) {
-          $(form).removeClass('loading');
-          var data = $.parseJSON(response);
-          if (data.status == 'success') {
-            // do something I can't test
-          } else {
-              $(form).find('[data-error]').html(data.message).show();
-          }
-        }
-      });
-    }
-
-    var validatePhone = {
-      required: true,
-      normalizer: function(value) {
-          var PHONE_MASK = '+X (XXX) XXX-XXXX';
-          if (!value || value === PHONE_MASK) {
-              return value;
-          } else {
-              return value.replace(/[^\d]/g, '');
-          }
-      },
-      minlength: 11,
-      digits: true
-    }
-
-    ////////
-    // FORMS
-
-
-    /////////////////////
-    // REGISTRATION FORM
-    ////////////////////
-    $(".js-registration-form").validate({
-      errorPlacement: validateErrorPlacement,
-      highlight: validateHighlight,
-      unhighlight: validateUnhighlight,
-      submitHandler: validateSubmitHandler,
-      rules: {
-        last_name: "required",
-        first_name: "required",
-        email: {
-          required: true,
-          email: true
-        },
-        password: {
-          required: true,
-          minlength: 6,
-        }
-        // phone: validatePhone
-      },
-      messages: {
-        last_name: "Заполните это поле",
-        first_name: "Заполните это поле",
-        email: {
-            required: "Заполните это поле",
-            email: "Email содержит неправильный формат"
-        },
-        password: {
-            required: "Заполните это поле",
-            email: "Пароль мимимум 6 символов"
-        },
-        // phone: {
-        //     required: "Заполните это поле",
-        //     minlength: "Введите корректный телефон"
-        // }
-      }
-    });
-  }
-
 
   //////////
   // BARBA PJAX
@@ -481,23 +207,6 @@ $(document).ready(function(){
     _window.scrollTop(0);
     $(window).scroll();
     $(window).resize();
-  }
-
-  //////////
-  // MEDIA Condition helper function
-  //////////
-  function mediaCondition(cond){
-    var disabledBp;
-    var conditionMedia = cond.substring(1);
-    var conditionPosition = cond.substring(0, 1);
-
-    if (conditionPosition === "<") {
-      disabledBp = _window.width() < conditionMedia;
-    } else if (conditionPosition === ">") {
-      disabledBp = _window.width() > conditionMedia;
-    }
-
-    return disabledBp
   }
 
   //////////
